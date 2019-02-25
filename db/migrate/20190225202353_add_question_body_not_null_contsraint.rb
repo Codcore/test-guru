a7092df06e2dba730e24b5704b63f3bb1e9e600d
Andrew Mark <codcore@gmail.com>
2019-02-25 22:34:46 +0200

    Add migrations with NOT NUL constraints for all newly created models

---
+++ db/migrate/20190225202353_add_question_body_not_null_contsraint.rb
@@ -0,0 +1,9 @@
+class AddQuestionBodyNotNullContsraint < ActiveRecord::Migration[5.2]
+  def up
+    change_column_null(:questions, :body, false)
+  end
+
+  def down
+    change_column_null(:questions, :body, true)
+  end
+end
