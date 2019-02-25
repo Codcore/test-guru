4c5135d60ddb378334a72ab7b2556ed15c94c23b
Andrew Mark <codcore@gmail.com>
2019-02-25 22:45:53 +0200

    Add migration with defaults value for answers.correct

--- db/migrate/20190225200615_create_answers.rb
+++ db/migrate/20190225200615_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.string :body
+      t.boolean :correct
       t.references :question, foreign_key: true
 
       t.timestamps
