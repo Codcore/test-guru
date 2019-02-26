12ac0f43a0bc53884dd2f4de023349e97c9450ad
Andrew Mark <codcore@gmail.com>
2019-02-27 01:02:11 +0200

    Add Test.all_from_category that returns all tests from given category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,10 @@
 class Test < ApplicationRecord
   has_and_belongs_to_many :users
+
+  def self.all_from_category(category_title)
+    category = Category.find_by(title: category_title)
+    return nil unless category
+
+    Test.where(category_id: category.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
 
-  def tests_at_level(level)
+  def tests_for_level(level)
     tests.where(level: level)
   end
 end
