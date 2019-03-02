13ba14826e010a9cfaa192406c7893a1a4b2b6e1
Andrew Mark <codcore@gmail.com>
2019-03-02 23:46:10 +0200

    Fix validating unique index on :title and :level for Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,14 +6,16 @@ class Test < ApplicationRecord
 
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than: 0 }
-  validates :title, uniqueness: true
+  validates :title, uniqueness: { scope: :level }
 
   scope :easy,     -> { where(level: 0..1) }
   scope :middle,   -> { where(level: 2..4) }
   scope :advanced, -> { where(level: 5..Float::INFINITY) }
-  scope :from_category, lambda { |category_title|
-    joins(:category)
-      .where(categories: { title: category_title })
-      .order(title: :desc).pluck(:title)
-  }
+
+
+  def self.from_category(category_title)
+    Test.joins(:category)
+        .where(categories: { title: category_title })
+        .order(title: :desc).pluck(:title)
+  end
 end
