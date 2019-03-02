2f07e1080ef4e146b36cf1ea70a8e3ebc8ed92b9
Andrew Mark <codcore@gmail.com>
2019-03-02 22:44:01 +0200

    Add validations for models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+
+  validates :body, presence: true
+  validate :max_answers
+
+  private
+
+  def max_answers
+    errors.add(:answers) unless (1..4).cover?(answers.count)
+  end
 end
