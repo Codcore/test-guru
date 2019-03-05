bcd251fa5bfa28c822179a7ea2c4ea6660e43f04
Andrew Mark <codcore@gmail.com>
2019-03-05 15:24:24 +0200

    Add QuestionsController#rescue_with_question_not_found

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
   before_action :find_test
   before_action :find_question, only: %i[show destroy]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index; end
 
@@ -33,5 +34,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
   end
 end
