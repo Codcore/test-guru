0e45775abfeb8f81e8fc23637774bfe43d7235e5
Andrew Mark <codcore@gmail.com>
2019-03-05 13:03:29 +0200

    Add action QuestionsController#index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index]
+
+  def index
+    questions = @test.questions.map(&:body).join("\n")
+    render plain: questions
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 end
