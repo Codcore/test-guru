bc2b419031326cc33ff1b834073d0ece65ab0a71
Andrew Mark <codcore@gmail.com>
2019-03-05 13:24:27 +0200

    Add action QuestionsController#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,22 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index]
+  before_action :find_test, only: %i[index, show]
+  before_action :find_question, only: %i[show]
 
   def index
-    questions = @test.questions.map(&:body).join("\n")
-    render plain: questions
+    render inline: '<h1><%= @test.title %></h1><ul><% @test.questions.each do |q| %><li><%= q.body %><% end %>'
+  end
+
+  def show
+    render plain: @question.body
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = @test.questions.all[params[:id].to_i - 1]
   end
 end
