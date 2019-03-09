0f9729a10380455c3e6b7fc0e2fee3bba11a361f
Andrew Mark <codcore@gmail.com>
2019-03-09 18:52:41 +0200

    Add TestsController CRUD and interface for it

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,39 @@
+class TestsController < ApplicationController
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def destroy
+    @test = Test.find(params[:id])
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+end
