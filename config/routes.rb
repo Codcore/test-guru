8ba34365991f1598aa20bee2a7dc373d4d3344db
Andrew Mark <codcore@gmail.com>
2019-03-11 19:42:59 +0200

    Part 1: Scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :answers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :tests, shallow: true do
-    resources :questions
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
