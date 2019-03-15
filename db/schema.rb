064439ed016fc9644432cbe6e2d48392e8ef947b
Andrew Mark <codcore@gmail.com>
2019-03-16 00:02:06 +0200

    Add password_digest to 'users' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_175939) do
+ActiveRecord::Schema.define(version: 2019_03_15_215918) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -60,11 +60,11 @@ ActiveRecord::Schema.define(version: 2019_03_11_175939) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
     t.integer "authored_tests"
+    t.string "password_digest"
   end
 
 end
