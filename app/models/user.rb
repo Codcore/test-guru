bd724bd5a9cb13b4c32c460eac1c0315180a6f96
Andrew Mark <codcore@gmail.com>
2019-03-16 01:29:58 +0200

    Finish part 1 : 'Authentication'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+  include Auth
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
-
-  validates :email, presence: true
+  has_secure_password
 
   scope :tests_for_level, ->(level) { tests.where(level: level) }
 
