b9c1345195f43f647a2f0b6b2fabfc70802f9e7f
Andrew Mark <codcore@gmail.com>
2019-03-16 16:45:51 +0200

    Use @current_user intead of find_user in TestsController

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_test, only: %i[show update edit destroy start]
-  before_action :find_user, only: :start
 
   def index
     @tests = Test.all
@@ -17,8 +16,8 @@ class TestsController < ApplicationController
 
   def start
     @test = Test.find(params[:id])
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    @current_user.tests.push(@test)
+    redirect_to @current_user.test_passage(@test)
   end
 
   def destroy
@@ -47,10 +46,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = User.first
   end
 
   def test_params
