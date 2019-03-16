483edd6d51bf991a17c8b46e36466e38cba06160
Andrew Mark <codcore@gmail.com>
2019-03-16 16:12:51 +0200

    Add logout logic and link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:email])
@@ -12,5 +11,10 @@ class SessionsController < ApplicationController
       alert = 'Are you a Guru? Verify your email and password please'
       redirect_to login_path, alert: alert
     end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
   end
 end
