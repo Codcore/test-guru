34a600a779633bcfeb804087459285e25f58b029
Andrew Mark <codcore@gmail.com>
2019-03-16 16:31:20 +0200

    Add callback 'authenticate_user!' for all resources

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,4 @@
 class TestsController < ApplicationController
-
   before_action :authenticate_user!
   before_action :find_test, only: %i[show update edit destroy start]
   before_action :find_user, only: :start
