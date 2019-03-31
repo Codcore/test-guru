5dddfe6ee3c912ad0400fe320fc6bfd8ecc2b904
Andrew Mark <codcore@gmail.com>
2019-03-31 19:58:19 +0300

    Fix lesson_16 due to the remarks

--- app/assets/javascripts/utilities/progress_bar.js
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -1,14 +1,18 @@
 document.addEventListener('turbolinks:load', function () {
     var progress_bar = document.querySelector('.progress-bar')
-    var question_number = progress_bar.dataset.questionId
-    var questions_count = progress_bar.dataset.questionsCount
-    var percentage      = (100/questions_count)*question_number
+    if (progress_bar) {
+        var question_number = progress_bar.dataset.questionNumber
+        var questions_count = progress_bar.dataset.questionsCount
+        var percentage      = (100 / questions_count) * (question_number - 1)
+        console.log(percentage + "%")
+        console.log(question_number)
 
-    progress_bar.classList.add('w-'+percentage)
-    progress_bar.setAttribute('role', 'progressbar')
-    progress_bar.setAttribute('aria-valuemin', '0')
-    progress_bar.setAttribute('aria-valuenow', percentage)
-    progress_bar.setAttribute('aria-valuemax', '100')
-    progress_bar.setAttribute('style', 'width: ' + percentage +'%')
-    progress_bar.textContent = percentage + '%'
+        progress_bar.classList.add('w-'+percentage)
+        progress_bar.setAttribute('role', 'progressbar')
+        progress_bar.setAttribute('aria-valuemin', '0')
+        progress_bar.setAttribute('aria-valuenow', percentage)
+        progress_bar.setAttribute('aria-valuemax', '100')
+        progress_bar.setAttribute('style', 'width: ' + percentage +'%')
+        progress_bar.textContent = percentage + '%'
+    }
 })
