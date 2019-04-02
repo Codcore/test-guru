858144fe0fd5d7128be41263511176e725dfa80c
Andrew Mark <codcore@gmail.com>
2019-04-02 15:41:27 +0300

    Fix mail settings to gmail.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     domain: 'gmail.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
+    authentication: 'login',
     enable_starttls_auto: true
   }
 
