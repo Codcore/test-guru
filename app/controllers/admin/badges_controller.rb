fd6659cb16a6df7290afc70fe18d9c41d4e716aa
Andrew Mark <codcore@gmail.com>
2019-04-08 18:46:48 +0300

    Set actions for BadgesController

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,10 +1,24 @@
 class Admin::BadgesController < Admin::BaseController
-  def index; end
+  before_action :authenticate_user!
+  before_action :find_badge, only: %i[edit update destroy]
+
+  def index
+    @badges = Badge.all
+  end
+
   def new; end
   def create; end
+  def edit; end
   def update; end
   def destroy; end
 
   private
 
+  def find_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:name, :file_name)
+  end
 end
--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,4 +1,9 @@
 class BadgesController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
+
+  def index
+    @badges = current_user.badges.all
+  end
+
   def show; end
 end
