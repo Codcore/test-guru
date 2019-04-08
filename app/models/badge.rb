2b624b23eda708ce98d2f0ceb3a95c8be162a444
Andrew Mark <codcore@gmail.com>
2019-04-08 19:11:41 +0300

    Add view for BadgeController#index, add Badge#file_url

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,4 +3,8 @@ class Badge < ApplicationRecord
 
   validates :name, uniqueness: true
   validates :file_name, presence: true
+
+  def file_url
+    File.join(Rails.root, 'public/badges_images', file_name)
+  end
 end
