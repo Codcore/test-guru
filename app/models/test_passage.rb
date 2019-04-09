be422fec8f0511ddd8d539a9805200688f10de61
Andrew Mark <codcore@gmail.com>
2019-04-09 17:32:50 +0300

    Fix current_question issue

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -7,7 +7,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
+  before_update :before_update_set_next_question, on: :update
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
