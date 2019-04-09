d6b92963f18d79058043b32aff1d19bc7726a306
Andrew Mark <codcore@gmail.com>
2019-04-09 17:21:20 +0300

    Fixes for heroku

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -63,6 +63,6 @@ class TestPassage < ApplicationRecord
   end
 
   def before_validation_set_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first if current_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
